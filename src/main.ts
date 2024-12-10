6704f4ddfe06354af152f2fc1fa2994d604440d1
Smae Tongmenglay <smaetongmenglay@MENGLAY.local>
2024-12-11 00:18:02 +0700

    added route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
+import "./assets/index.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import "./assets/index.css";
+import router from "./router";
 
-const pinia = createPinia();
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(pinia);
+app.use(router);
+
 app.mount("#app");
